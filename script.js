ec17317e5ae562111ac51de9622a75159e558b79
potato-farm-k <superdoogoo@naver.com>
2026-06-16 17:26:11 +0900

    Fix workflow help button and modal image

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Potato’s Day v0.2");
+console.log("Potato’s Day v0.2.1");
 
 const workflowOpenButton = document.querySelector("[data-workflow-open]");
 const workflowModal = document.querySelector("#workflow-modal");
